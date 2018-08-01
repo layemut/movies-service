830b5115120b817b761abe2609ec7d817eb20619
Özcan Candağ <ozcancandag@gmail.com>
2018-08-01 08:50:13 +0300

    wildfly plugin eklendi

--- src/main/java/tr/com/layemut/moviesservice/controller/PersonController.java
+++ src/main/java/tr/com/layemut/moviesservice/controller/PersonController.java
@@ -1,5 +1,6 @@
 package tr.com.layemut.moviesservice.controller;
 
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,6 +94,23 @@ public class PersonController {
         personCreateResponse.setPerson(createdPerson);
 
         return ResponseEntity.ok().body(personCreateResponse);
+    }
+
+    @GetMapping("/delete")
+    public ResponseEntity delete(@RequestParam("userName") String userName) {
+
+        Long person = personRepository.deleteByUserName(userName);
+        Result result = new Result();
+
+        if (person == 1) {
+            result.setCode(200);
+            result.setMessage(String.format("%s isimli kullanıcı başarı ile silindi.", userName));
+        } else {
+            result.setCode(404);
+            result.setMessage(String.format("%s isimli kullanıcı bulunamadı.", userName));
+        }
+
+        return ResponseEntity.status(result.getCode()).body(result);
     }
 
 
--- src/main/java/tr/com/layemut/moviesservice/repository/PersonRepository.java
+++ src/main/java/tr/com/layemut/moviesservice/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package tr.com.layemut.moviesservice.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import tr.com.layemut.moviesservice.entity.Person;
 
@@ -10,4 +11,7 @@ public interface PersonRepository extends MongoRepository<Person, ObjectId> {
     Person findByUserNameAndPassword(@Param("userName") String userName, @Param("password") String password);
 
     Person findByUserName(@Param("userName") String userName);
+
+    @Query(value = "{userName : ?0}", delete = true)
+    Long deleteByUserName(@Param("userName") String userName);
 }
