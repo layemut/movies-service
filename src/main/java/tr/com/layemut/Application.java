3ee8e58a1f59ed563d694229e5b3911e8bed6973
Özcan Candağ <ozcan.candag@tegsoft.com>
2019-07-12 08:06:02 +0300

    removed test

--- src/main/java/tr/com/layemut/Application.java
+++ src/main/java/tr/com/layemut/Application.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Test test!");
+    public static void main(String[] args){ 
         SpringApplication.run(Application.class, args);
     }
 }
