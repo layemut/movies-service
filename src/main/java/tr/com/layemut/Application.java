d83c0acf0e22a4ad643ad4ebeb4b3c6296cbd746
OZCAN CANDAG (Bt Strateji ve Yonetisim Bsk. - Analist Programci(Bt)) <O_CANDAG@thynet.thy.com>
2019-08-27 10:18:35 +0300

    minor changes to application and application.properties

--- src/main/java/tr/com/layemut/Application.java
+++ src/main/java/tr/com/layemut/Application.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args){
-        if(true){
-            //TTEAST
-        }
         SpringApplication.run(Application.class, args);
     }
 }
